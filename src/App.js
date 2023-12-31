1fafdcedba39699a3efe69cbb9af926c9171f4d7
sumit <sumitkanth999@gmail.com>
2023-12-31 12:17:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './style.css';
+import div_gif from './gif_img.gif';
 
-function App() {
+const App = () => {
+
+  const gif = () => {
+    const h1 = document.querySelector('h1');
+    const btn = document.querySelector('button');
+    const img = document.querySelector('img');
+    const h2 = document.querySelector('h2');
+    h1.style.display = "none";
+    btn.style.display = "none";
+    img.style.display = "inline";
+    h2.style.display = "inline-block";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+            <h1> Happy Wala <span> New Year </span> 🤡</h1>
+            <button onClick={gif}>Click On Me</button>
+            <div className="gif_div">
+              <img src={div_gif}/>
+              <br />
+              <h2>ese hi haste rahio hamesha 🤣🤣 </h2>
+            </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
